7c5ade31f87444519be6ccef333c9b889bbbc2f3
Nick Allen <Nick Allen>
2017-05-08 19:27:30 -0500

    first commit

---
+++ examplejson.js
@@ -0,0 +1,58 @@
+{
+    "city": {
+        "id": 2643743,
+        "name": "London",
+        "coord": {
+            "lon": -0.1258,
+            "lat": 51.5085
+        },
+        "country": "GB",
+        "population": 0
+    },
+    "cod": "200",
+    "message": 1.2294498,
+    "cnt": 2,
+    "list": [{
+        "dt": 1494241200,
+        "temp": {
+            "day": 280.7,
+            "min": 277.83,
+            "max": 280.7,
+            "night": 277.83,
+            "eve": 280.7,
+            "morn": 280.7
+        },
+        "pressure": 1033.15,
+        "humidity": 65,
+        "weather": [{
+            "id": 800,
+            "main": "Clear",
+            "description": "sky is clear",
+            "icon": "01n"
+        }],
+        "speed": 3.63,
+        "deg": 36,
+        "clouds": 0
+    }, {
+        "dt": 1494327600,
+        "temp": {
+            "day": 286.06,
+            "min": 277.29,
+            "max": 287.53,
+            "night": 279.78,
+            "eve": 287.16,
+            "morn": 277.29
+        },
+        "pressure": 1028.66,
+        "humidity": 71,
+        "weather": [{
+            "id": 801,
+            "main": "Clouds",
+            "description": "few clouds",
+            "icon": "02d"
+        }],
+        "speed": 1.76,
+        "deg": 49,
+        "clouds": 24
+    }]
+}
